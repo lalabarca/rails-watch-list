3c42f6adf86d45a2230914a7653ea95a6d351ea5
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2021-12-14 13:22:42 -0400

    1ère version terminée

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to root_path
     else
       render :new
     end
@@ -25,7 +25,7 @@ class ListsController < ApplicationController
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :list_id, uniqueness: true
+  validates :list_id, uniqueness: { scope: :movie_id }
 end
